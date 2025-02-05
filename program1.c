6692ca097945c156fb270e6177a68ea836d21c76
unknown <dhanasrithangaraj2005@gmail.com>
2025-02-06 04:22:48 +0530

    solved problems

---
+++ program1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a,sum;
+    printf("enter the num:");
+    scanf("%d",&a);
+    sum=a+2;
+    printf("the sum of %d  is:%d",a,sum);
+}
---
+++ program2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a,difference;
+    printf("enter the num:");
+    scanf("%d",&a);
+    difference=a-5;
+    printf("the difference of %d  is:%d",a,difference);
+}
