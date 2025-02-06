c86a417dbc6a1b25c9a1c02add98a89512226c77
unknown <dhanasrithangaraj2005@gmail.com>
2025-02-07 03:59:20 +0530

    problems

---
+++ program3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a,multiply;
+    printf("enter the num:");
+    scanf("%d",&a);
+    multiply=a*5;
+    printf("the multiply of %d  is:%d",a,multiply);
+}
---
+++ program4.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a,divide;
+    printf("enter the num:");
+    scanf("%d",&a);
+    divide=a%5;
+    printf("the multiply of %d  is:%d",a,divide);
+}
